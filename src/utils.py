03459a17d72b10ba64b7a1f093230089943f4bb3
Vishwa7989 <vishwajeet7989@gmail.com>
2026-02-27 23:09:20 -0500

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,28 @@
 import os
 import sys
-import dill
 
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok = True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
-    
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-def evaluate_models(x_train, y_train,x_test,y_test,models,param):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -30,18 +30,17 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=3)
-            gs.fit(x_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(x_train, y_train)
+            model.fit(X_train,y_train)
 
-        
-            #model.fit(x_train, y_train)  # Train model
+            #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(x_train)
+            y_train_pred = model.predict(X_train)
 
-            y_test_pred = model.predict(x_test)
+            y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
 
@@ -52,5 +51,13 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)    
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
